7620e58157f238c8c7a1d3df5c56158f7f0d733d
B.G.Adrian <aditza8@gmail.com>
2018-11-18 16:11:38 +0200

    try to fix travis gocov and go 1

---
+++ travis.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+#go 1.0 fail: cannot use test profile flag with multiple packages
+[[ -v COVERAGE ]] && go test -coverprofile=coverage.txt -covermode=atomic ./... || go test ./...
