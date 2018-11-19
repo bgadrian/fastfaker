d02099d42e2d74affdabe013914dda2b3e7c3dfe
B.G.Adrian <aditza8@gmail.com>
2018-11-19 20:14:40 +0200

    small documentation updates

--- faker/doc.go
+++ faker/doc.go
@@ -1,5 +1,9 @@
 /*
 Package faker contains a random data generator (faker) with over 130 functions and has 2 modes: fast or (concurrent) safe.
+Can be used to mock data, test or stress test your systems or databases.
+You can generate almost anything, from names, addresses to prices and vehicle transmission types.
+
+If you are looking for a HTTP service with the same functionality, not a package see Pseudoservice, it is a wrapper on FastFaker https://github.com/bgadrian/pseudoservice
 
 Usage:
 	import "github.com/bgadrian/fastfaker/faker"
@@ -12,11 +16,12 @@ Method 2, instantiate a concurrent safe version:
 	fmt.Println("Email:", faker.Email()) //kimsteuber@jones.com
 Method 3, create a fast faker that is Not safe to use from multiple goroutines:
 	faker := NewFastFaker()
-	fmt.Println(Global.BeerName()) //Duvel
+	fmt.Println(Global.Template("{name} drinks {beername}.")) //Adrian drinks Duvel.
 
 Fakers take their data from the github.com/bgadrian/fastfaker/data package.
 The pseudo-random algorithm is provided by the github.com/bgadrian/fastfaker/randomizer
 package, that is a wrapper of the rand package from the standard library.
 
+For a more advanced overview of the Template feature see https://github.com/bgadrian/fastfaker/blob/master/TEMPLATES.md
 */
 package faker
